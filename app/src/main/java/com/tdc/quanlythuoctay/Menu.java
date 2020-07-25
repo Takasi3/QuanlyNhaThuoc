dc6fc58d14ad6f3268152a128534860afaaa42f9
takasimika <levandai97@gmail.com>
2020-07-25 20:09:08 +0700

    Lên

--- app/src/main/java/com/tdc/quanlythuoctay/Menu.java
+++ app/src/main/java/com/tdc/quanlythuoctay/Menu.java
@@ -7,40 +7,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class Login extends AppCompatActivity {
-private Spinner spinerLanguage;
-private Button btnLogin;
+public class Menu extends AppCompatActivity {
+private ListView listView;
+private Button btndisplaying;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        // Ánh xạ
-
-        spinerLanguage = (Spinner) findViewById(R.id.spinerLanguage);
-        btnLogin = (Button) findViewById(R.id.btn_login);
-
+        setContentView(R.layout.activity_menu);
+        //Ánh Xạ
+        listView = (ListView) findViewById(R.id.lsvBXH);
+        btndisplaying =(Button)findViewById(R.id.displaying);
         ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Tiếng Việt");
-        arrayList.add("English");
+        arrayList.add("#1 : Nhà Thuốc An Khang");
+        arrayList.add("#2 : Nhà Thuốc Long Châu");
+        arrayList.add("#3 : Nhà Thuốc Sao Việt");
+        arrayList.add("#4 : Nhà Thuốc Minh Phương");
+
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 arrayList );
-        spinerLanguage.setAdapter(arrayAdapter);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        listView.setAdapter(arrayAdapter);
+        btndisplaying.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this, Menu.class));
+                startActivity(new Intent(Menu.this, lstPharmacy.class));
                 // close splash activity
-                finish();
             }
         });
+
+
 
     }
 }
